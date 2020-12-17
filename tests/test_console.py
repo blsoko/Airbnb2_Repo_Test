0c31306868eb0586ac37a9c5be2d4e3230c59fca
devarias <gedafu2005@gmail.com>
2020-12-17 16:16:32 -0500

    Test for console and HBNBCommand

--- tests/test_console.py
+++ tests/test_console.py
@@ -23,14 +23,14 @@ class tests_Console(unittest.TestCase):
 
     def test_console_module_docstring(self):
         """test docstring of the console.py"""
-        self.assertIsNot(console.__doc__, None,
+        self.assertIsNot("console".__doc__, None,
                          "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
+        self.assertTrue(len("console".__doc__) >= 1,
                         "console.py needs a docstring")
 
     def test_HBNBCommand_class_docstring(self):
         """tests docstring of the HBNBCommand classes"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
+        self.assertIsNot("HBNBCommand".__doc__, None,
                          "HBNBCommand class needs a docstring")
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+        self.assertTrue(len("HBNBCommand".__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
